d68cfa23274215b139145435e7b67c9d6b2dda77
huxc5 <xianchun.hu@accenture.com>
2018-01-26 10:24:10 +0800

    boot

--- boot/src/main/java/com/boot/HomeController.java
+++ boot/src/main/java/com/boot/HomeController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
