9908e94e2f50cd959e16ad27bfc302cf539b31a1
e-martin.hu <e-martin.hu@homecredit.cn>
2019-06-25 15:12:42 +0800

    boot

--- boot/src/main/java/com/boot/HomeController.java
+++ boot/src/main/java/com/boot/HomeController.java
@@ -1,11 +1,17 @@
 package com.boot;
 
+import com.boot.common.unit.XmlUtils;
 import com.boot.user.model.User;
 import com.boot.user.service.UserService;
+import org.dom4j.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -18,9 +24,28 @@ public class HomeController {
     private UserService userService;
 
     @RequestMapping("/home")
-    public String home(){
-        List<User> users = userService.getAll();
-        System.out.println(users);
-        return "home page";
+    public String home(HttpServletRequest request){
+//        List<User> users = userService.getAll();
+//        System.out.println(users);
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+        String code = request.getParameter("code");
+        System.out.println("code:" + code);
+        System.out.println("echostr:" + echostr);
+        return echostr;
+    }
+
+    @PostMapping("/xml")
+    @ResponseBody
+    public User getXml(HttpServletRequest request) {
+        Document document = XmlUtils.parseToDocument(request);
+        String xml = document.asXML();
+
+        User user = new User();
+        user.setName("aaa");
+        user.setEmail("a@163.com");
+        return user;
     }
 }
